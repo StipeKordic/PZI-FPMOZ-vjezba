b2b8a26c61b09c529472268f64d0eb034226d56e
Stipeee <stipe.kordic@fpmoz.sum.ba>
2022-11-15 16:57:57 +0100

    update3

--- index.php
+++ index.php
@@ -13,12 +13,21 @@
   <body>
     <h1>
       <?php
-      $test = "Testna varijabla";
+        $test = "Testna varijabla";
         echo "Stipeee";
         echo $test;
         var_dump($test);
       ?>
     </h1>
+    <form method="post">
+      Name: <input type="text" name="name"><br>
+      E-mail: <input type="text" name="email"><br>
+      <input type="submit">
+    </form>
+    <?php
+        var_dump($_POST)
+        //var_dump($_GET) ovo je kad radimo get metodu
+    ?>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
